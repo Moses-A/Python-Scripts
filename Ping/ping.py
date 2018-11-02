22bfa3a03aaec09bdbc26878f053be86ac4f391b
Moses Arocha <carocha24@gmail.com>
2018-11-01 19:32:37 -0500

    Update ping.py

--- Ping/ping.py
+++ Ping/ping.py
@@ -8,34 +8,32 @@ import sys
 import time
 
 
-def ping(inputfile):
+def ping(inputfile, loop):
    cwd = os.getcwd()
    path = (cwd+'/'+inputfile)
    #path = (cwd+'\\'+'IP.txt')
    with open(path, encoding='utf-8') as f:
       data = f.read().splitlines()
-
    array_length = len(data)
-   while 1:
+   while loop:
       for i in range(array_length):
          hostname = data[i]
          response = os.system("ping -c 1 " + hostname)
-
          if response == 0:
             print(hostname, 'Is Up!')
          else:
             print(hostname, 'Is Down!')
       time.sleep(15)
-    inputfile.close()
 
 
 def main(argv):
    inputfile = ''
+   loop = 1
 
    try:
       opts, args = getopt.getopt(argv, "hi:o:", ["ifile="])
    except getopt.GetoptError:
-      print ('ping.py -i <inputfile>')
+      print ('ping.py -i <inputfile> -l')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h' or opt == '-H':
@@ -43,8 +41,10 @@ def main(argv):
          sys.exit()
       elif opt in ("-i", "--input"):
          inputfile = arg
+      elif opt in ("-l", "--loop"):
+         inputfile = arg
    print ('Input file is: ', inputfile)
-   ping(inputfile)
+   ping(inputfile, loop)
 
 
 if __name__ == "__main__":
